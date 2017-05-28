e006147ee85a24ad468cc939869a4121f19c8f35
jebbard <jensebert@gmx.net>
2017-05-28 16:43:35 +0200

    Update moin--get-list-item-info for further commands to implement

--- moin-lists.el
+++ moin-lists.el
@@ -45,8 +45,15 @@ blank lines or lines with only whitespaces between two list items. Furthermore,
 as soon as there is at least one blank or tab at the beginning of a line, it is 
 considered to belong to a list, if a previous line contains a list. Only lines 
 with a non-whitespace character as first character of the line break the list."
-  (setq moin-var-current-list-item-info (moin--list-get-item-info))
-  (if (eq nil moin-var-current-list-item-info)
+  ;; Usually we like to avoid doing the same thing twice, but here:
+  ;; First any list related command in moin-mode.el will call this function
+  ;; to determine whether in a list, and then any sub-function called by it
+  ;; will again call `moin--list-get-item-info', resulting in one additional,
+  ;; but unnecessary call. However, the alternatives would either mean to break
+  ;; with the common structure of the rest of the mode (tables and headings), or
+  ;; introducing strange global variables. As most modern computers should be
+  ;; able to handle this thing, we decided to call `moin--list-get-item-info' twice.
+  (if (eq nil (moin--list-get-item-info))
       nil
     t))
 
@@ -56,31 +63,38 @@ with a non-whitespace character as first character of the line break the list."
 
 (defun moin--list-get-item-info()
   "Retrieves information about the current list item where point is on. Always 
-expects to currently be in a list, otherwise it reacts with a user error.
+expects to currently be in a list, so any caller must ensure that this is actually
+the case. An item info always only refers to the current item and does not care
+whether this item is part of the subtree of another item, or it has a subtree itself.
 
 The returned information is: 
-(start-pos preamble-end-pos whitespace-before starting-bullet), where
-* start-pos is the start position of the item, i.e. the beginning of the line 
+(start-pos preamble-end-pos whitespace-before starting-bullet end-pos), where
+* start-pos is the start point of the item, i.e. the beginning of the line 
 where the starting bullet or number of the list is located on
-* preamble-end-pos is the end position of the preamble, i.e. the text including 
+* preamble-end-pos is the end point of the preamble, i.e. the text including 
 whitespace, starting bullet or number and any further whitspace before the actual 
 item text starts
 * whitespace-before contains any whitespace before the starting bullet or number
 * starting-bullet contains the text of the starting bullet or number without any 
-whitespace, including a trailing dot for numbers"
+whitespace, including a trailing dot for numbers
+* end-pos is the end point of the item, i.e. the point after the last character
+(excluding last newline) of the item; this IS NOT the point after the last subtree
+item of the current item, i.e. if the current item has sub-items, it is the point
+at eol before the first subitem."
 (let (start-pos
       preamble-end-pos
       whitespace-before-bullet
       whitespace-after-bullet
       starting-bullet
+      end-pos
       really-in-list-p
+      eob-p
       (list-regex
        "^\\([\t ]+\\)\\([*.]\\|\\([1-9]+\\|[A-Za-z]\\)\\.\\)\\([\t ]*\\)\\([^[:space:]].*?$\\|$\\)"))
   
   (save-excursion
     (beginning-of-line)
-    ;; This match string usually only covers the first line of an item! The sole case
-    ;; where it spans two lines is that after the 
+    
     (while (and (looking-at "^\\s-") (not (looking-at list-regex)) (not (bobp)))
       (previous-line)
       (beginning-of-line))
@@ -99,10 +113,35 @@ whitespace, including a trailing dot for numbers"
     (if really-in-list-p
 	(progn
 	  (save-excursion
-	    ;; TODO
 
-	    )
-	  (list start-pos preamble-end-pos whitespace-before-bullet starting-bullet whitespace-after-bullet))
+	    (setq eob-p nil)
+	    (end-of-line)
+
+	    (if (not (eobp))
+		(progn
+		  (next-line)
+		  (beginning-of-line)
+		  
+		  (while (and (looking-at "^\\s-") (not (looking-at list-regex)) (not eob-p))
+		    (end-of-line)
+		    (if (eobp)
+			(setq eob-p t)
+		      (progn
+			(next-line)
+			(beginning-of-line))))))
+
+	    (if (not eob-p)
+		(progn
+		  (beginning-of-line)
+		  (if (not (bobp))
+		      (progn
+			(previous-line)
+			(end-of-line)))))
+	    
+	    (setq end-pos (point)))
+	  
+	  (list start-pos preamble-end-pos whitespace-before-bullet starting-bullet
+		whitespace-after-bullet end-pos))
       ;; else return nil
       nil)))
 
@@ -118,11 +157,11 @@ function if point is currently not in a list."
 	item-whitespace-after
 	item-info)
 
-    (unless (moin-is-in-list-p)
-      (user-error "Not in list currently"))
-
     (setq item-info (moin--list-get-item-info))
     
+    (if (not item-info)
+      (user-error "Not in list currently"))
+
     (setq item-start-pos (car item-info))
     (setq item-preamble-end-pos (car (cdr item-info)))
     (setq item-whitespace-before (car (cdr (cdr item-info))))
