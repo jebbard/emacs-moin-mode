557e619a020e51e98ddce2b02d996f8d90dfc0ca
jebbard <jensebert@gmx.net>
2017-08-08 21:12:35 +0200

    Reengineered most of the syntax highlighting except multiline code environments

--- moin-faces.el
+++ moin-faces.el
@@ -73,32 +73,32 @@ of tables, set to nil otherwise. Default is t"
 ; ------
 ; Highlighting Formatted Text
 ; -------
-(defface moin-face-bold '((t (:foreground "LightSkyBlue" :height 1.4 :weight bold)))
-  "Face name to use for bold text in moinmoin"
+(defface moin-face-bold '((t (:weight bold)))
+  "Face to use for bold text in moinmoin"
   :group 'moin-faces)
-(defface moin-face-italic '((t (:foreground "Orange" :height 0.7 :slant italic)))
-  "Face name to use for italic text in moinmoin"
+(defface moin-face-italic '((t (:slant italic)))
+  "Face to use for italic text in moinmoin"
   :group 'moin-faces)
 (defface moin-face-underline '((t (:underline t)))
-  "Face name to use for underlined text in moinmoin"
+  "Face to use for underlined text in moinmoin"
   :group 'moin-faces)
 (defface moin-face-stroke '((t (:strike-through t)))
-  "Face name to use for stroked text in moinmoin"
+  "Face to use for stroked text in moinmoin"
   :group 'moin-faces)
 (defface moin-face-subscript '((t (:height 0.8)))
-  "Face name to use for subscripts in moinmoin"
+  "Face to use for subscripts in moinmoin"
   :group 'moin-faces)
 (defface moin-face-superscript '((t (:height 0.8)))
-  "Face name to use for superscripts in moinmoin"
+  "Face to use for superscripts in moinmoin"
   :group 'moin-faces)
 (defface moin-face-monospace '((t (:family "Courier")))
-  "Face name to use for typewriter text in moinmoin"
+  "Face to use for monospace text in moinmoin"
   :group 'moin-faces)
 (defface moin-face-larger '((t (:height 1.2)))
-  "Face name to use for larger text in moinmoin"
+  "Face to use for larger text in moinmoin"
   :group 'moin-faces)
 (defface moin-face-smaller '((t (:height 0.8)))
-  "Face name to use for smaller text in moinmoin"
+  "Face to use for smaller text in moinmoin"
   :group 'moin-faces)
 
 ; ------
@@ -107,41 +107,41 @@ of tables, set to nil otherwise. Default is t"
 (defface moin-face-table-separator
   '((((class color) (background light)) (:foreground "Blue1"))
     (((class color) (background dark)) (:foreground "LightSkyBlue")))
-  "Face name to use for separation of columns in tables of moinmoin"
+  "Face to use for separation of columns in tables of moinmoin"
   :group 'moin-faces)
 (defface moin-face-table-content
   '((((class color) (background light)) (:foreground "Blue1"))
     (((class color) (background dark)) (:foreground "LightSkyBlue")))
-  "Face name to use for separation of columns in tables of moinmoin"
+  "Face to use for tables content in moinmoin"
   :group 'moin-faces)
 (defface moin-face-table-processing-instruction
   '((((class color) (background light)) (:foreground "DarkGray" :weight bold))
     (((class color) (background dark)) (:foreground "Gray70" :weight bold)))
-  "Face name to use for processing instructions in tables of moinmoin"
+  "Face to use for processing instructions in tables of moinmoin"
   :group 'moin-faces)
 
 ; ------
 ; Highlighting Macros
 ; -------
 (defface moin-face-macro-content '((t (:foreground "Dark Blue")))
-  "Face name to use for names of macros in moinmoin"
+  "Face to use for names of macros in moinmoin"
   :group 'moin-faces)
 (defface moin-face-macro-braces
   '((((class color) (background light)) (:foreground "Gray70"))
     (((class color) (background dark)) (:foreground "DarkGray")))
-  "Face name to use for parameters of macros in moinmoin"
+  "Face to use for macros braces in moinmoin"
   :group 'moin-faces)
 
 ; ------
 ; Highlighting Embeddings (e.g. attachments)
 ; -------
 (defface moin-face-embedding-content '((t (:foreground "Orange")))
-  "Face name to use for names of macros in moinmoin"
+  "Face to use for cointent of embeddings in moinmoin"
   :group 'moin-faces)
 (defface moin-face-embedding-braces
   '((((class color) (background light)) (:foreground "Gray70"))
     (((class color) (background dark)) (:foreground "DarkGray")))
-  "Face name to use for parameters of macros in moinmoin"
+  "Face to use embedding braces in moinmoin"
   :group 'moin-faces)
 
 ; ------
@@ -150,27 +150,27 @@ of tables, set to nil otherwise. Default is t"
 (defface moin-face-h1
   '((((class color) (background light)) (:height 1.4 :foreground "Blue1"))
     (((class color) (background dark)) (:height 1.4 :foreground "LightSkyBlue")))
-  "Face name to use for 1-level headings in moinmoin"
+  "Face to use for level 1 headings in moinmoin"
   :group 'moin-faces)
 (defface moin-face-h2
   '((((class color) (background light)) (:height 1.3 :foreground "DarkGoldenrod"))
     (((class color) (background dark)) (:height 1.3 :foreground "LightGoldenrod")))
-  "Face name to use for 2-level headings in moinmoin"
+  "Face to use for level 2 headings in moinmoin"
   :group 'moin-faces)
 (defface moin-face-h3
   '((((class color) (background light)) (:height 1.2 :foreground "Purple"))
     (((class color) (background dark)) (:height 1.2 :foreground "Cyan1")))
-  "Face name to use for 3-level headings in moinmoin"
+  "Face to use for level 3 headings in moinmoin"
   :group 'moin-faces)
 (defface moin-face-h4
   '((((class color) (background light)) (:height 1.1 :foreground "Firebrick"))
     (((class color) (background dark)) (:height 1.1 :foreground "chocolate1")))
-  "Face name to use for 4-level headings in moinmoin"
+  "Face to use for level 4 headings in moinmoin"
   :group 'moin-faces)
 (defface moin-face-h5
   '((((class color) (background light)) (:foreground "ForestGreen"))
     (((class color) (background dark)) (:foreground "PaleGreen")))
-  "Face name to use for 5-level headings in moinmoin"
+  "Face to use for level 5 headings in moinmoin"
   :group 'moin-faces)
 
 ; ------
@@ -178,27 +178,27 @@ of tables, set to nil otherwise. Default is t"
 ; -------
 
 (defconst moin-face-env-light-fg "grey30"
-  "The foreground color to use in a code environment in case of light background")
+  "Face to use in a code environment in case of light background")
 (defconst moin-face-env-dark-fg "white"
-  "The foreground color to use in a code environment in case of dark background")
+  "Face to use in a code environment in case of dark background")
 
 (defface moin-face-env
   '((((class color) (background light)) (:foreground "grey30" :inherit 'moin-face-monospace))
     (((class color) (background dark)) (:foreground "white" :inherit 'moin-face-monospace)))
-  "Face name to use for code inside braces in moinmoin"
+  "Face to use for code inside braces in moinmoin"
   :group 'moin-faces)
 (defface moin-face-env-braces
   '((((class color) (background light)) (:foreground "Gray70"))
     (((class color) (background dark)) (:foreground "DarkGray")))
-  "Face name to use for baces which delimit environments in moinmoin"
+  "Face to use for baces which delimit environments in moinmoin"
   :group 'moin-faces)
 (defface moin-face-env-parser '((t (:foreground "plum3" :weight bold)))
-  "Face name to use for parser specs"
+  "Face to use for parser specs"
   :group 'moin-faces)
 (defface moin-face-env-usual-bg
   '((((class color) (background light)) (:background "black" :inherit 'moin-face-monospace))
     (((class color) (background dark)) (:background "white" :inherit 'moin-face-monospace)))
-  "Face name to use for parser specs"
+  "Face to use for usual code env background"
   :group 'moin-faces)
 
 ; ------
@@ -207,78 +207,84 @@ of tables, set to nil otherwise. Default is t"
 (defface moin-face-link
     '((((class color) (background light)) (:foreground "Purple"))
       (((class color) (background dark)) (:foreground "Cyan")))
-  "Face name to use for rules in moinmoin"
+  "Face to use for link content in moinmoin"
   :group 'moin-faces)
 (defface moin-face-email '((t (:inherit 'moin-face-link :underline t)))
-  "Face name to use for emails in moinmoin"
+  "Face to use for emails in moinmoin"
   :group 'moin-faces)
 (defface moin-face-link-brackets '((t (:inherit 'moin-face-link :weight bold)))
-  "Face name to use for rules in moinmoin"
+  "Face to use for link brackets in moinmoin"
   :group 'moin-faces)
 (defface moin-face-followable-link '((t (:inherit 'moin-face-link :underline t)))
-  "Face name to use for rules in moinmoin"
+  "Face to use for a url in moinmoin"
   :group 'moin-faces)
 (defface moin-face-wiki-word '((t (:foreground "blue4" :weight bold)))
-  "Face name to use for CamelCase links in moinmoin"
+  "Face to use for WikiWords in moinmoin"
   :group 'moin-faces)
 
 ; ------
 ; Highlighting various other elements
 ; -------
 (defface moin-face-rule '((t (:foreground "tomato2" :weight bold)))
-  "Face name to use for rules in moinmoin"
+  "Face to use for rules in moinmoin"
   :group 'moin-faces)
 (defface moin-face-comment '((t (:foreground "maroon3")))
-  "Face name to use for comments in moinmoin"
+  "Face to use for comments in moinmoin"
   :group 'moin-faces)
 (defface moin-face-variable '((t (:inherit 'moin-face-monospace)))
-  "Face name to use for code inside braces in moinmoin"
-  :group 'moin-faces)
-(defface moin-face-processing-instruction '((t (:foreground "maroon3")))
-  "Face name to use for comments in moinmoin"
+  "Face to use for variables in moinmoin"
   :group 'moin-faces)
 
 ;; ==================================================
 ;; "Private" Functions
 (defun moin--create-dynamic-bg-face(bg-color-name)
-  "This function dynamically creates a new face symbol for the given 
-background color name and sets the background color in the returned face.
-It can be used to dynamically change the background color of a piece of 
-fontified buffer based on its contents (e.g. background color of a table based
-on the specified wiki color). It returns a default background color if the given
-color could not be identified (e.g. during typing)."
-  (if (color-defined-p bg-color-name)
-     (progn
-       ; Get the face symbol if already defined before
-       (setq face-symbol (intern-soft bg-color-name))
+  "This function dynamically creates a new face symbol for the given
+BG-COLOR-NAME and sets the background color in the returned face. It
+can be used to dynamically change the background color of a piece of
+fontified buffer based on its contents (e.g. background color of a
+table based on the specified wiki color). It returns a default
+background color if the given color could not be identified (e.g.
+during typing)."
+  (let (face-symbol
+	rgb-bg-color
+	r
+	g
+	b
+	fg-color-name)
 
-       ; Or create it newly if it wasn't
-       (if face-symbol nil
-   	(progn
-   	  (setq face-symbol (intern (concat "moin-face-env-" bg-color-name)))))
-  
-       (face-spec-set face-symbol '((t (:inherit 'moin-face-env-usual-bg))))
+    (if (color-defined-p bg-color-name)
+	(progn
+					; Get the face symbol if already defined before
+	  (setq face-symbol (intern-soft bg-color-name))
 
-       (setq rgb-bg-color (color-name-to-rgb bg-color-name))
-       (setq r (car rgb-bg-color))
-       (setq g (car (cdr rgb-bg-color)))
-       (setq b (car (cdr (cdr rgb-bg-color))))
+					; Or create it newly if it wasn't
+	  (if face-symbol nil
+	    (progn
+	      (setq face-symbol (intern (concat "moin-face-env-" bg-color-name)))))
+	  
+	  (face-spec-set face-symbol '((t (:inherit 'moin-face-env-usual-bg))))
 
-       ; Set the foreground color based on the "darkness" of the bg color
-       (if (> (+ r g b) 1.5)
-          (setq fg-color-name moin-face-env-light-fg)
-   	  (setq fg-color-name moin-face-env-dark-fg))
-      
-       (set-face-background face-symbol bg-color-name)
-       (set-face-foreground face-symbol fg-color-name)
-       ;; Return the newly created face symbol  
-       face-symbol)
-     ;; If no defined color, return the default background color
-     'moin-face-env-usual-bg))
+	  (setq rgb-bg-color (color-name-to-rgb bg-color-name))
+	  (setq r (car rgb-bg-color))
+	  (setq g (car (cdr rgb-bg-color)))
+	  (setq b (car (cdr (cdr rgb-bg-color))))
+
+					; Set the foreground color based on the "darkness" of the bg color
+	  (if (> (+ r g b) 1.5)
+	      (setq fg-color-name moin-face-env-light-fg)
+	    (setq fg-color-name moin-face-env-dark-fg))
+	  
+	  (set-face-background face-symbol bg-color-name)
+	  (set-face-foreground face-symbol fg-color-name)
+	  ;; Return the newly created face symbol  
+	  face-symbol)
+      ;; If no defined color, return the default background color
+      'moin-face-env-usual-bg)))
 
 
 (defun moin--setup-font-lock ()
-  "Installs all font lock patterns for syntax highlighting in their required order"
+  "Installs all font lock patterns for syntax highlighting in their
+required order"
   (setq font-lock-multiline t)
   (make-local-variable 'font-lock-extra-managed-props)
   (add-to-list 'font-lock-extra-managed-props 'display)
@@ -346,15 +352,34 @@ color could not be identified (e.g. during typing)."
      ;;  (1 'moin-face-env-braces prepend)
      ;;  (2 'moin-face-env t)
      ;;  (3 'moin-face-env-braces prepend))
+
+     ;; *******
+     ;; Highlighting Macros
+     ;;				
+     ;; Requirements:
+     ;; - Macros start and end on the same line
+     ;; - Macros are highlighted if within formatted text
+     ;; - They are highlighted in tables, but not in code environments or headings
+     ;; *******
+     ;; Macro
+     ("\\(<<\\)\\(.*?\\)\\(>>\\)"
+      (1 'moin-face-macro-braces keep)
+      (2 'moin-face-macro-content keep)
+      (3 'moin-face-macro-braces keep))
     
      ;; *******
      ;; Highlighting Formatted Text
      ;;				
      ;; Requirements:
-     ;; - Formattings might overlap each other and must be correctly highlighted
+     ;; - Formatting that overlaps each other is only supported for bold and italic
+     ;;   i.e. starting a new formatting inside an existing formatted text might
+     ;;   not be highlighted correctly
      ;; - Formatting is not highlighted in headings, environments or links
      ;; - Formatting is highlighted in tables
-     ;; - Formatting never crosses multiple lines
+     ;; - Although MoinMoin seems to support formatting crossing multiple lines,
+     ;;   this mode does not support this
+     ;; - Known issue: Subscripts and superscripts are always highlighted, even in
+     ;;   headings and environments
      ;; *******
      ;; Bold in italic
      ("\\(''.*?\\)\\('''.*?'''\\)\\(.*?''\\)"
@@ -394,13 +419,65 @@ color could not be identified (e.g. during typing)."
      ("\\(~-.*?-~\\)"
       (1 'moin-face-smaller keep))
 
-     ;; ------
+     ;; *******
+     ;; Highlighting Embeddings (e.g. Attachment)
+     ;;				
+     ;; Requirements:
+     ;; - Embeddings cannot span multiple lines
+     ;; - Embeddings are also highlighted in tables
+     ;; *******
+     ("\\([^{]{{\\)\\([^{].*?[^}]\\)\\(}}[^}]\\)"
+      (1 'moin-face-embedding-braces keep)
+      (2 'moin-face-embedding-content keep)
+      (3 'moin-face-embedding-braces keep))
+    
+     ;; *******
+     ;; Highlighting Links
+     ;;				
+     ;; Requirements:
+     ;; - Freehand links are highlighted, but only until the first whitespace character
+     ;; - Links are also highlighted in tables
+     ;; - They cannot span multiple lines
+     ;; *******
+     ;; Freehand absolute URL
+ ("\\(http\\|https\\|ftp\\|nntp\\|news\\|mailto\\|telnet\\|wiki\\|file\\|irc\\)\\(://[A-Za-z0-9_-+&?%#:./=;$]+\\)"
+      (1 'moin-face-followable-link keep)
+      (2 'moin-face-followable-link keep))
+     ;; EMail
+     ("\\([A-Za-z0-9_+-]+@[A-Za-z0-9_-]+\\.[A-Za-z0-9_-]+\\)"
+      (1 'moin-face-email keep))
+     ;; Explicit Link
+     ("\\(\\[\\[\\)\\(.*?\\)\\(\\]\\]\\)"
+      (1 'moin-face-link-brackets keep)
+      (2 'moin-face-link keep)
+      (3 'moin-face-link-brackets keep))
+     ;; Moin WikiWord    
+     ("\\(?:^\\|[^A-Za-z!]\\)\\(\\(?:\\.\\./\\)?/?[A-Z][a-z]+[A-Z][a-z][A-Za-z]*\\(?:/[A-Z][a-z]+[A-Z][a-z][A-Za-z]*\\)?\\)"
+      (1 'moin-face-wiki-word keep))
+    
+     ;; *******
+     ;; Highlighting various other elements
+     ;;				
+     ;; Requirements:
+     ;; - Comments and variables are higlighted in tables, but not in headings or code environments
+     ;; - They must not span multiple lines
+     ;; *******
+     ;; Horizontal rules
+     ("-\\{4,\\}" (0 'moin-face-rule keep))
+     ;; Inline commentsm
+     ("\\(/\\*.*?\\*/\\)"
+      (1 'moin-face-comment keep))
+     ;; Variables
+     ("\\(@[a-zA-Z0-9]*?@\\)"
+      (1 'moin-face-variable keep))
+
+     ;; *******
      ;; Highlighting Tables
      ;;				
      ;; Requirements:
      ;; - Table delimiters must be at start and end of line to be highlighted as table
      ;; - Formatting, Links, Wikiwords, Macros and Variables are highlighted in tables
-     ;; -------
+     ;; *******
      ;; Table content until next separator incl. table processing instructions
      ("||\\(<.*?>\\)"
       (1 'moin-face-table-processing-instruction append))
@@ -410,62 +487,6 @@ color could not be identified (e.g. during typing)."
      ("\\(.*?\\)\\(||\\)"
       (1 'moin-face-table-content append)
       (2 'moin-face-table-separator append))
-
-     ;; ------
-     ;; Highlighting Macros
-     ;;				
-     ;; Requirements:
-     ;; - Table delimiters must be at start and end of line to be highlighted as table
-     ;; - Formatting, Links, Wikiwords, Macros and Variables are highlighted in tables
-     ;; -------
-     ;; ;; Macro
-     ;; ("\\(<<\\)\\(.*?\\)\\(>>\\)"
-     ;;  (1 'moin-face-macro-braces)
-     ;;  (2 'moin-face-macro-content prepend)
-     ;;  (3 'moin-face-macro-braces))
-
-     ;; ; ------
-     ;; ; Highlighting Embeddings (e.g. Attachment)
-     ;; ; -------
-     ;; ;; Embedding
-     ;; ("\\([^{]{{\\)\\([^{].*?[^}]\\)\\(}}[^}]\\)"
-     ;;  (1 'moin-face-embedding-braces)
-     ;;  (2 'moin-face-embedding-content prepend)
-     ;;  (3 'moin-face-embedding-braces))
-    
-     ;; ; ------
-     ;; ; Highlighting Links
-     ;; ; -------
-     ;; ;; Freehand absolute URL
-     ;; ("\\(http\\|https\\|ftp\\|nntp\\|news\\|mailto\\|telnet\\|wiki\\|file\\|irc\\)\\(://[A-Za-z0-9_-+&?%#:./=;$]+\\)"
-     ;;  (1 'moin-face-followable-link t)
-     ;;  (2 'moin-face-followable-link t))
-     ;; ;; EMail
-     ;; ("\\([A-Za-z0-9_+-]+@[A-Za-z0-9_-]+\\.[A-Za-z0-9_-]+\\)"
-     ;;  (1 'moin-face-email t))
-     ;; ;; Explicit Link
-     ;; ("\\(\\[\\[\\)\\(.*?\\)\\(\\]\\]\\)"
-     ;;  (1 'moin-face-link-brackets t)
-     ;;  (2 'moin-face-link keep)
-     ;;  (3 'moin-face-link-brackets t))
-     ;; ;; Moin WikiWord    
-     ;; ("\\(?:^\\|[^A-Za-z!]\\)\\(\\(?:\\.\\./\\)?/?[A-Z][a-z]+[A-Z][a-z][A-Za-z]*\\(?:/[A-Z][a-z]+[A-Z][a-z][A-Za-z]*\\)?\\)"
-     ;;  (1 'moin-face-wiki-word t))
-    
-     ;; ; ------
-     ;; ; Highlighting various other elements
-     ;; ; -------
-     ;; ;; Horizontal rules
-     ;; ("-\\{4,\\}" (0 'moin-face-rule t))
-     ;; ;; Inline commentsm
-     ;; ("\\(/\\*.*?\\*/\\)"
-     ;;  (1 'moin-face-comment t))
-     ;; ;; Variables
-     ;; ("\\(@[a-zA-Z0-9].*?@\\)"
-     ;;  (1 'moin-face-variable t))
-     ;; ;; Processing instructions
-     ;; ("\\(^#.*$\\)"
-     ;;  (1 'moin-face-processing-instruction t))
     ) 'set))
 
 
